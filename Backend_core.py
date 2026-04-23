9403a98352effe41e7779acee1bbc753e0923759
Rajat Thakur <rajatthakur6125@gmail.com>
2026-04-23 23:11:24 +0530

    Implement CORS support in after_request
    Add CORS headers to responses for API requests.

--- Backend_core.py
+++ Backend_core.py
@@ -28,6 +28,11 @@ CORS(app, resources={
         ]
     }
 })
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+    return response
 
 #OPTIONAL PROTECTED ROUTE DECORATOR -------------------- #
 def token_required(f):
