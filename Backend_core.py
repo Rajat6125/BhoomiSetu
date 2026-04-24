fbf451e079ddd0d24c9fade841f5f4a8a384627c
Rajat <rajatthakur6125@gmail.com>
2026-04-24 16:40:58 +0530

    ..

--- Backend_core.py
+++ Backend_core.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from supabase import create_client
 from flask_cors import CORS
 from dotenv import load_dotenv
-from ml_service import register_ml_routes
+# from ml_service import register_ml_routes
 import os
 import jwt
 import datetime
@@ -225,7 +225,7 @@ def health():
 
 
 # -------------------- REGISTER ML ROUTES -------------------- #
-register_ml_routes(app)
+# register_ml_routes(app)
 
 # -------------------- RUN -------------------- #
 if __name__ == "__main__":
